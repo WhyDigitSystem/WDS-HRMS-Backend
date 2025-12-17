dcdf0f38a3a6d6a1aefe663e4af2f5962d787f73
narasimhawds <narashima@whydigit.in>
2025-12-17 15:23:39 +0530

    changes

--- src/main/java/com/efit/hrms/entity/JobPostingsVO.java
+++ src/main/java/com/efit/hrms/entity/JobPostingsVO.java
@@ -1,23 +1,16 @@
 package com.efit.hrms.entity;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.efit.hrms.dto.CreatedUpdatedDate;
 import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -43,19 +36,30 @@ public class JobPostingsVO {
 	private String department;
 	@Column(name = "location")
 	private String location;
-	
+
 	@Column(name = "orgid")
 	private Long orgId;
-	
+
 	@Column(name = "branch")
 	private String branch;
 	@Column(name = "branchcode")
 	private String branchCode;
-	
+
+	@Column(name = "skills")
+	private String skills;
+	@Column(name = "description")
+	private String description;
+	@Column(name = "keywords")
+	private String keywords;
+	@Column(name = "education")
+	private String education;
+	@Column(name = "experience")
+	private String experience;
+
 	@Column(name = "active")
 	private boolean active;
 	@Column(name = "cancel")
-	private boolean cancel=false;
+	private boolean cancel = false;
 	@Column(name = "createdby")
 	private String createdBy;
 	@Column(name = "modifiedby")
@@ -65,13 +69,12 @@ public class JobPostingsVO {
 
 	@Column(name = "screenname", length = 25)
 	private String screenName = "JOB POSTINGS";
-	
+
 	@JsonGetter("active")
 	public String getActive() {
 		return active ? "Active" : "In-Active";
 	}
-	
-	
+
 	@Embedded
 	@Builder.Default
 	private CreatedUpdatedDate commonDate = new CreatedUpdatedDate();
