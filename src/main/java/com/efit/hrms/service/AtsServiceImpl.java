276416b7a5d525ef1a4e1d9a31ac83ed023bba0c
Nandha <nandha@whydigit.in>
2026-01-03 10:13:10 +0530

    bug fix

--- src/main/java/com/efit/hrms/service/AtsServiceImpl.java
+++ src/main/java/com/efit/hrms/service/AtsServiceImpl.java
@@ -30,7 +30,8 @@ public class AtsServiceImpl implements AtsService {
 
     private static final String PYTHON_ATS_URL =
 //            "http://127.0.0.1:8000/api/v1/ats/analyze";
-    "http://139.5.190.244:8000/api/v1/ats/analyze";
+//            "http://139.5.190.244:8000/api/v1/ats/analyze";
+    		  "http://139.5.190.73:8000/api/v1/ats/analyze";
     @Override
     @Transactional
     public AtsResponseDTO processAts(
