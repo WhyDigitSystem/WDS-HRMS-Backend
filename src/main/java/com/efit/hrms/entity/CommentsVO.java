8d048457ddc0180dd405ca6303e6a309e3952f88
Dhinesh <dhinesh@whydigit.in>
2026-05-14 14:17:04 +0530

    commit comment bug

--- src/main/java/com/efit/hrms/entity/CommentsVO.java
+++ src/main/java/com/efit/hrms/entity/CommentsVO.java
@@ -48,14 +48,10 @@ public class CommentsVO {
 	@Column(name = "ticketid")
 	private Long ticketId;
 	
-	@Column(name="sourceusername")
-	private String sourceUserName;
+	
 	
 	@Column(name="sourceticketid")
 	private Long sourceTicketId;
-	
-	@Column(name="sourceid")
-	private Long sourceId;
 
 	
 	@Column(name="notificationflag")
--- src/main/java/com/efit/hrms/service/CommentSyncService.java
+++ src/main/java/com/efit/hrms/service/CommentSyncService.java
@@ -114,4 +114,5 @@ public class CommentSyncService {
 	        e.printStackTrace();
 	    }
 	}
+}
 
