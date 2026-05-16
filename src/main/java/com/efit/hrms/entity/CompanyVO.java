7e7832d35881fb2fcbf389cd9e8084bf28f7e671
Dhinesh <dhinesh@whydigit.in>
2026-05-16 11:51:33 +0530

    employee code generation api

--- src/main/java/com/efit/hrms/entity/CompanyVO.java
+++ src/main/java/com/efit/hrms/entity/CompanyVO.java
@@ -133,6 +133,12 @@ public class CompanyVO {
 	@Column(name = "companylogo", columnDefinition = "LONGBLOB")
 	private byte[] companyLogo;
 	
+	@Column(name = "elastnum")
+	private int eLastNum = 1;
+	
+	@Column(name = "clastnum")
+	private int cLastNum = 1;
+	
 	@OneToMany(mappedBy = "companyVO", cascade = CascadeType.ALL)
 	@JsonManagedReference
 	private List<CompanyWeekOffVO>companyWeekOffVO;
